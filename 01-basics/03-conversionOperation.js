28424fa00d700c0c47dfe496506daaec3c8ab558
Aaryash08 <113716203+Aaryash08@users.noreply.github.com>
2023-10-10 23:00:35 +0530

    Create 03-conversionOperation.js
    Conversion of datatypes

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,29 @@
+//let score=undefined;
+// let score=null;
+//let score="33";
+// let score="33abc";
+// let score=true;
+// let score=false;
+let score="Aaryash"
+// console.log(typeof score);
+// console.log(typeof(score));
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33"->33
+//"33abc"->NaN
+//true->1; false->0
+
+let isLoggedIn="Aaryash";
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1->true; 0->false
+//""->false
+//"Aaryash->true"
+
+let  someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
